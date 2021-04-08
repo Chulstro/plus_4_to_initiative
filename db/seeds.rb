a804cf0eba7f561dab57ce81c05be6fae46368e0
Chandler Hulstrom <chulstro11@gmail.com>
2021-04-08 15:20:44 -0600

    test for race index complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Character.create(name: "Tielfang", lvl: 3)
+Character.create(name: "Chitter", lvl: 5)
