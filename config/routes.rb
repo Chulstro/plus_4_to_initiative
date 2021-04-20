30caa42e1c51f1a85e54508a014b02842f23b130
Chandler Hulstrom <chulstro11@gmail.com>
2021-04-20 10:57:02 -0600

    Completed basic creation functionality for a character

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/', to: 'characters#index'
+  get '/new', to: 'characters#new'
+  post '/characters', to: 'characters#create'
 
   get '/races', to: 'races#index'
 
--- spec/features/characters/new_spec.rb
+++ spec/features/characters/new_spec.rb
@@ -12,15 +12,15 @@ RSpec.describe 'When creating a new character' do
     expect(page).to_not have_content("Chitter")
     click_on "New Character"
 
-    expect(current_path).to equal("/new")\
+    expect(current_path).to eq("/new")
 
     fill_in 'name', with: "Chitter"
-    fill_in 'lvl', with: "3"
+    select '3', from: "lvl"
     select 'Barbarian', from: 'class'
     select 'Kenku', from: 'race'
 
     click_on "Create Character"
-    expect(current_path).to equal("/")
+    expect(current_path).to eq("/")
     expect(page).to have_content("Chitter")
   end
 end
